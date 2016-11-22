2ecd3a9c7a7e8e27f2ac40efd0c1aa274f089b55
Victor Sonora Pombo <victor.pombo@mytechia.com>
2016-11-22 16:32:02 +0100

    Uses a more persistent folder for log and con files.

--- conf/wificonfig.sh
+++ conf/wificonfig.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #exec_path="python `python -c "import site; print site.getsitepackages()[0]"`/wifi_control/__main__.py"
 #eval ${exec_path}
-python -m wifi_control.__main__ /var/tmp/wificonfig_data.p /var/tmp/wificonfig.log
+mkdir -p /var/local/tmp
+python -m wifi_control.__main__ /var/local/tmp/wificonfig_data.p /var/local/tmp/wificonfig.log
